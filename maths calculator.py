37ffba6eb550802bad899bce8c6498ad76c71be0
Sanika Joshi <113428223+Sanika8903@users.noreply.github.com>
2022-10-25 20:15:49 +0530

    Create maths calculator.py

---
+++ maths calculator.py
@@ -0,0 +1,11 @@
+#program to calculate subtraction, additon, multiplication and division of two numbers
+a = int(input("enter first number\n"))
+b = int(input("enter second number\n"))
+c = a + b
+d = a - b
+e = a * b
+f = a \ b
+print("the addition is",c)
+print("the subtraction is",d)
+print("the multiplication is",e)
+print("the division is",f)
